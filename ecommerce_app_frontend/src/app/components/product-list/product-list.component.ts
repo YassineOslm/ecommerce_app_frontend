44afb01892a70ed526aab3125af16ac6b812cd25
Yassine Aarab <yassino1996@hotmail.com>
2024-07-15 20:28:28 +0200

    search product by keyword implemented

--- ecommerce_app_frontend/src/app/components/product-list/product-list.component.ts
+++ ecommerce_app_frontend/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  searchMode: boolean = false;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -23,7 +24,29 @@ export class ProductListComponent implements OnInit {
   }
 
   listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
+    if(this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProduct();
+    }
+
+  }
+  handleSearchProducts() {
+
+    const theKeyWord: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyWord).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+
+  }
+
+  handleListProduct() {
     //check if "id" parameter in available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
--- ecommerce_app_frontend/src/app/services/product.service.ts
+++ ecommerce_app_frontend/src/app/services/product.service.ts
@@ -20,11 +20,7 @@ export class ProductService {
 
     //need to build URL based on the category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-
-
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    );
+    return this.getProducts(searchUrl);
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
@@ -33,6 +29,19 @@ export class ProductService {
     );
   }
 
+  searchProducts(theKeyWord: string): Observable<Product[]> {
+    //need to build URL based on the keyword
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyWord}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
 }
 
 interface GetResponseProducts {
